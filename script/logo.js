9470a85f36bf8e531a68b852bf2262850b731603
Md Niaz Al Motin <niaz.al.motin@gmail.com>
2025-06-09 14:26:46 +0600

    Add files via upload

--- script/logo.js
+++ script/logo.js
@@ -31,3 +31,72 @@ document.addEventListener("DOMContentLoaded", function() {
         updateLogos();
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        "asset/poster/poster1.jpg",
+        "asset/poster/poster2.jpg",
+        "asset/poster/poster3.jpg",
+        "asset/poster/poster4.jpg",
+        "asset/poster/poster5.jpg"
+    ];
+
+    let start = 0;
+
+    const poster1 = document.getElementById("poster1");
+    const poster2 = document.getElementById("poster2");
+    const poster3 = document.getElementById("poster3");
+
+    function updateposters() {
+        poster1.style.backgroundImage = `url('${images[start]}')`;
+        poster2.style.backgroundImage = `url('${images[(start + 1) % images.length]}')`;
+        poster3.style.backgroundImage = `url('${images[(start + 2) % images.length]}')`;
+    }
+
+    updateposters();
+
+    document.getElementById("posterRight").addEventListener("click", function() {
+        start = (start + 1) % images.length;
+        updateposters();
+    });
+
+    document.getElementById("posterLeft").addEventListener("click", function() {
+        start = (start - 1 + images.length) % images.length;
+        updateposters();
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        "asset/tn/tn1.jpg",
+        "asset/tn/tn2.jpeg",
+        "asset/tn/tn3.jpeg",
+        "asset/tn/tn4.jpg",
+        "asset/tn/tn5.jpg"
+    ];
+
+    let start = 0;
+
+    const tn1 = document.getElementById("tn1");
+    const tn2 = document.getElementById("tn2");
+    const tn3 = document.getElementById("tn3");
+
+    function updatetns() {
+        tn1.style.backgroundImage = `url('${images[start]}')`;
+        tn2.style.backgroundImage = `url('${images[(start + 1) % images.length]}')`;
+        tn3.style.backgroundImage = `url('${images[(start + 2) % images.length]}')`;
+    }
+
+    updatetns();
+
+    document.getElementById("tnRight").addEventListener("click", function() {
+        start = (start + 1) % images.length;
+        updatetns();
+    });
+
+    document.getElementById("tnLeft").addEventListener("click", function() {
+        start = (start - 1 + images.length) % images.length;
+        updatetns();
+    });
+});
